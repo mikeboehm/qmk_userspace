a653fe98855f5dcab0d6c3642d29fa46bd3863d3
Mike Boehm <mike@myzeller.com>
2024-12-15 10:33:51 +1100

    Revert Via layout import
    This reverts commit 2754b415df1f3a54e945df4b89c60be2f14915c1.

--- keyboards/crkbd/keymaps/via/keymap.c
+++ keyboards/crkbd/keymaps/via/keymap.c
@@ -95,48 +95,15 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
- KC_GRV,
-KC_Q,
-KC_W,
-KC_F,
-KC_P,
-KC_B,
-KC_TAB,
-MT(MOD_LSFT,KC_A),
-MT(MOD_LCTL,KC_R),
-MT(MOD_LALT,KC_S),
-MT(MOD_LGUI,KC_T),
-KC_G,
-KC_ESC,
-KC_Z,
-KC_X,
-KC_C,
-KC_D,
-KC_V,
-KC_LGUI,
-MO(1),
-KC_SPC,
-KC_BSPC,
-KC_SCLN,
-KC_Y,
-KC_U,
-KC_L,
-KC_J,
-KC_QUOT,
-MT(MOD_LSFT | MOD_RSFT,KC_O),
-MT(MOD_LCTL | MOD_RCTL,KC_I),
-MT(MOD_LALT,KC_E),
-MT(MOD_LGUI | MOD_RGUI,KC_N),
-KC_M,
-KC_ESC,
-KC_SLSH,
-KC_DOT,
-KC_COMM,
-KC_H,
-KC_K,
-KC_BSPC,
-MO(2),
-KC_ENT
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LGUI, TL_LOWR,  KC_SPC,     KC_ENT, TL_UPPR, KC_RALT
+                                      //`--------------------------'  `--------------------------'
 
   ),
 
