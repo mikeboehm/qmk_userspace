c4b1e4fbb72e8eda504d69738eebb80af7c14a6a
Mike Boehm <mike@boehm.com.au>
2024-12-22 14:47:10 +1100

    Fix rotation

--- keyboards/crkbd/keymaps/via/keymap.c
+++ keyboards/crkbd/keymaps/via/keymap.c
@@ -126,10 +126,10 @@ bool oled_task_user(void) {
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (is_keyboard_left()) {
-        // return OLED_ROTATION_180;
-        return OLED_ROTATION_0; // flips the display 180 degrees if offhand
+        return OLED_ROTATION_270; // layer state
     }
-    return OLED_ROTATION_270;
+    
+    return OLED_ROTATION_270;  // Image
     // return rotation;
 }
 
